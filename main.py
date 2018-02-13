cc5abf2ee474aa344993115daf73d5adde16cc91
Nikita Popov <mailto.nikitapopov@gmail.com>
2018-02-13 22:09:31 +0300

    update

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import discogs_client
 import musicbrainzngs
 import pandas
 import pylast
+import requests
+import spotipy
+from datetime import datetime
 from pprint import pprint
 
 # last.fm
@@ -14,7 +17,11 @@ password_hash = pylast.md5('')  # @todo fill password
 musicbrainzngs.set_useragent("new-albums", "0.0.1", "https://github.com/nikitalpopov/new-albums")
 
 # discogs
-discogs = discogs_client.Client('new-albums/0.0.1', user_token='tnKwPJHZUkDdOTwQWdLRZEYWziNRQhBoKnLjDumm')
+user_token = 'tnKwPJHZUkDdOTwQWdLRZEYWziNRQhBoKnLjDumm'
+discogs = discogs_client.Client('new-albums/0.0.1', user_token=user_token)
+
+# spotify
+spotify = spotipy.Spotify()
 
 # run script
 api = pylast.LastFMNetwork(api_key=api_key,
@@ -22,35 +29,83 @@ api = pylast.LastFMNetwork(api_key=api_key,
                            username=username)
 artists = pylast.User(username, api).get_library().get_artists(limit=None)
 
+artists = [artist.item.name for artist in artists if artist.playcount >= 20]
 discogs_id = []
 musicbrainz_id = []
+spotify_id = []
 for artist in artists:
-    if artist.playcount >= 20:
-        musicbrainz_search = musicbrainzngs.search_artists(artist=artist.item.name)['artist-list']
-        discogs_search = discogs.search(artist.item.name, type='artist')
+    print(artist)
+    try:
+        musicbrainz_search = musicbrainzngs.search_artists(artist=artist)['artist-list']
 
         if musicbrainz_search:
             musicbrainz_id.append(musicbrainz_search[0]['id'])
+
+            # get albums
             # pprint(musicbrainzngs.get_artist_by_id(musicbrainz_search[0]['id'],
             #                                        includes=["release-groups"],
             #                                        release_type=["album", "ep"])['artist']['release-group-list'])
         else:
             musicbrainz_id.append(None)
+    except:
+        musicbrainz_id.append(None)
+
+    try:
+        discogs_search = discogs.search(artist, type='artist')
 
         if discogs_search:
             discogs_id.append(str(discogs_search[0].id))
+
+            # get albums
+            # releases = requests.get('https://api.discogs.com/artists/' + discogs_id[-1] + '/releases'
+            #                         + '?sort=year'
+            #                         + '&sort_order=desc')
+            # pprint(releases.json()['releases'])
+            # print()
+            # releases = requests.get('https://api.discogs.com/database/search'
+            #                         + '?artist=' + artist
+            #                         + '&year=' + str(datetime.now().year - 1)
+            #                         + '&format=album'
+            #                         + '&type=release'
+            #                         + '&token=' + user_token)
+            # pprint(releases.json()['results'])
             # for release in discogs_search[0].releases:
             #     pprint(release)
         else:
             discogs_id.append(None)
-    else:
-        musicbrainz_id.append(None)
+    except:
         discogs_id.append(None)
 
-data = {'artist': [artist.item.name for artist in artists],
+    try:
+        spotify_search = spotify.search(q='artist:' + artist, type='artist')
+
+        if spotify_search:
+            spotify_id.append(spotify_search['artists']['items'][0]['id'])
+
+            # get albums
+            # albums = []
+            # results = spotify.artist_albums(spotify_id[-1], album_type='album')
+            # albums.extend(results['items'])
+            # while results['next']:
+            #     results = spotify.next(results)
+            #     albums.extend(results['items'])
+            # seen = set()  # to avoid dups
+            # albums.sort(key=lambda album: album['name'].lower())
+            # for album in albums:
+            #     name = album['name']
+            #     if name not in seen:
+            #         print((' ' + name))
+            #         seen.add(name)
+        else:
+            spotify_id.append(None)
+    except:
+        spotify_id.append(None)
+
+data = {'artist': artists,
         'musicbrainz_id': musicbrainz_id,
-        'discogs_id': discogs_id}
-dataframe = pandas.DataFrame.from_dict(data).dropna(how='any')
+        'discogs_id': discogs_id,
+        'spotify_id': spotify_id}
+dataframe = pandas.DataFrame.from_dict(data)
 dataframe.to_csv('library.csv', sep=',', encoding='utf-8')
 
 # musicbrainzngs.get_artist_by_id(artist_id, includes=["release-groups"], release_type=["album", "ep"])
